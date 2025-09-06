93141ad5fecc49ee0ed1f3246b31c0e4b3d4ec76
GreenKookie56 <brandon05121@gmail.com>
2025-09-06 11:33:03 +0800

    Add files via upload

--- jokers/overclock.lua
+++ jokers/overclock.lua
@@ -2,54 +2,49 @@ SMODS.Joker{ --Overclock
     key = "overclock",
     config = {
         extra = {
+            money÷1 = 1,
             ignore = 0
         }
     },
     loc_txt = {
         ['name'] = 'Overclock',
         ['text'] = {
-            [1] = 'Create a {C:dark_edition}Negative{}',
-            [2] = '{C:attention}Boss Farming Guide{}',
-            [3] = 'at the end of {C:green}shop{}'
+            [1] = 'Create a {C:dark_edition}Negative{} {C:attention}Boss Farming Guide{}',
+            [2] = 'when {C:green}shop{} is rerolled',
+            [3] = '{X:legendary,C:white}^1{} Mult for every {C:money}${} owned'
         },
         ['unlock'] = {
             [1] = ''
         }
     },
     pos = {
-        x = 5,
+        x = 4,
         y = 3
     },
     display_size = {
         w = 71 * 1, 
         h = 95 * 1
     },
-    cost = 30,
-    rarity = "sholium_peculiar",
+    cost = 17,
+    rarity = "sholatro_the_pro",
     blueprint_compat = true,
-    demicoloncompat = true,
     eternal_compat = true,
     perishable_compat = false,
     unlocked = true,
     discovered = true,
     atlas = 'CustomJokers',
-    in_pool = function(self, args)
-          return (
-          not args 
-          or args.source ~= 'buf' and args.source ~= 'jud' and args.source ~= 'rif' and args.source ~= 'rta' and args.source ~= 'sou' and args.source ~= 'uta' and args.source ~= 'wra' 
-          or args.source == 'sho'
-          )
-          and true
-      end,
+    loc_vars = function(self, info_queue, card)
+        return {vars = {card.ability.extra.ignore, card.ability.extra.money÷1 + ((math.floor(lenient_bignum(G.GAME.dollars)) or 0))}}
+    end,
 
     calculate = function(self, card, context)
-        if context.ending_shop or context.forcetrigger then
+        if context.reroll_shop  then
                 return {
                     func = function()
             local created_joker = true
             G.E_MANAGER:add_event(Event({
                 func = function()
-                    local joker_card = SMODS.add_card({ set = 'Joker', key = 'j_sholium_bossfarmingguide' })
+                    local joker_card = SMODS.add_card({ set = 'Joker', key = 'j_sholatro_bossfarmingguide' })
                     if joker_card then
                         joker_card:set_edition("e_negative", true)
                         
@@ -64,6 +59,11 @@ SMODS.Joker{ --Overclock
             end
             return true
         end
+                }
+        end
+        if context.cardarea == G.jokers and context.joker_main and lenient_bignum(G.GAME.dollars) >= 1 then
+                return {
+                    e_mult = card.ability.extra.money÷1 + (math.floor(lenient_bignum(G.GAME.dollars)))
                 }
         end
     end
